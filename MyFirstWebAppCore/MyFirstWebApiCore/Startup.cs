5a9d0cdece1af759bfba193e447d2505a1be8e96
Ivan Zitko <ivanzitko@gmail.com>
2019-04-21 21:45:25 +0200

    Zavrsen tutorijal

--- MyFirstWebAppCore/MyFirstWebApiCore/Startup.cs
+++ MyFirstWebAppCore/MyFirstWebApiCore/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using MyFirstWebApiCore.Models;
 
 namespace MyFirstWebApiCore
 {
@@ -25,6 +27,10 @@ namespace MyFirstWebApiCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
